0fbc07679160701521726bb5ab73d96ae5370a79
global667 <global667@web.de>
2024-05-30 17:09:00 +0200

    Bugfix the GiveTipp feature

--- sources/uci.cpp
+++ sources/uci.cpp
@@ -92,15 +92,16 @@ void UCI::readData() {
                 auto fy = (mv.at(1) - '0');
                 auto tx = (mv.at(2) - 'a');
                 auto ty = (mv.at(3) - '0');
-                basemodel.fromUCI.x = fy;
-                basemodel.fromUCI.y = fx;
-                basemodel.toUCI.x = ty;
-                basemodel.toUCI.y = tx;
+
                 if (tipp == false) {
+                    basemodel.fromUCI.x = fy;
+                    basemodel.fromUCI.y = fx;
+                    basemodel.toUCI.x = ty;
+                    basemodel.toUCI.y = tx;
                     // Ruft gameloop auf
                     //emit updateView(Point(fy, fx), Point(ty, tx), BaseModel::Mode::human);
                 } else {
-                    //emit giveTipp(Point(fy, fx), Point(ty, tx));
+                    emit giveTipp(Point(fy, fx), Point(ty, tx));
                 }
             } else if (c.contains("info")) {
                 qDebug() << c;
